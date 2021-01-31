0097c105f688f915fe0ece57af554260c9033086
zhiyuan8019 <2393957799@qq.com>
2021-01-31 17:49:24 +0800

    添加了number的测试例

--- test.cpp
+++ test.cpp
@@ -16,63 +16,119 @@ static int test_pass = 0;
             test_pass++;\
         else {\
             fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
-            main_ret = 1;                                \
+            main_ret = 1;\
          }\
     } while(0)
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+#define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
+#define TEST_NUMBER(expect, json)\
+    do {\
+        Json_Parse v;\
+        EXPECT_EQ_INT(JSON_PARSE_OK,  v.json_parse(json));\
+        EXPECT_EQ_INT(JSON_NUMBER, v.json_get_type());\
+        EXPECT_EQ_DOUBLE(expect, v.json_get_number());\
+    } while(0)
+
+#define TEST_ERROR(error, json)\
+    do {\
+        Json_Parse v;\
+        EXPECT_EQ_INT(error,v.json_parse(json));\
+        EXPECT_EQ_INT(JSON_NULL, v.json_get_type());\
+    } while(0)
+
+#define TEST_EQUAL(except, json)\
+    do {\
+        Json_Parse v;\
+        EXPECT_EQ_INT(JSON_PARSE_OK,v.json_parse(json));\
+        EXPECT_EQ_INT(except, v.json_get_type());\
+    } while(0)
 
 static void test_parse_null() {
-    Json_Parse v;
-    EXPECT_EQ_INT(JSON_PARSE_OK, v.json_parse("null"));
-    EXPECT_EQ_INT(JSON_NULL, v.json_get_type());
-
+    TEST_EQUAL(JSON_NULL,"null");
 }
 static void test_parse_false() {
-    Json_Parse v;
-    EXPECT_EQ_INT(JSON_PARSE_OK, v.json_parse("false"));
-    EXPECT_EQ_INT(JSON_FALSE, v.json_get_type());
-
+    TEST_EQUAL(JSON_FALSE,"false");
 }
 static void test_parse_true() {
-    Json_Parse v;
-    EXPECT_EQ_INT(JSON_PARSE_OK, v.json_parse("true"));
-    EXPECT_EQ_INT(JSON_TRUE, v.json_get_type());
+    TEST_EQUAL(JSON_TRUE,"true");
 }
 
+static void test_parse_number() {
+    TEST_NUMBER(0.0, "0");
+    TEST_NUMBER(0.0, "-0");
+    TEST_NUMBER(0.0, "-0.0");
+    TEST_NUMBER(1.0, "1");
+    TEST_NUMBER(-1.0, "-1");
+    TEST_NUMBER(1.5, "1.5");
+    TEST_NUMBER(-1.5, "-1.5");
+    TEST_NUMBER(3.1416, "3.1416");
+    TEST_NUMBER(1E10, "1E10");
+    TEST_NUMBER(1e10, "1e10");
+    TEST_NUMBER(1E+10, "1E+10");
+    TEST_NUMBER(1E-10, "1E-10");
+    TEST_NUMBER(-1E10, "-1E10");
+    TEST_NUMBER(-1e10, "-1e10");
+    TEST_NUMBER(-1E+10, "-1E+10");
+    TEST_NUMBER(-1E-10, "-1E-10");
+    TEST_NUMBER(1.234E+10, "1.234E+10");
+    TEST_NUMBER(1.234E-10, "1.234E-10");
+    TEST_NUMBER(0.0, "1e-10000"); /* must underflow */
+    TEST_NUMBER(1.0000000000000002, "1.0000000000000002"); /* the smallest number > 1 */
+    TEST_NUMBER( 4.9406564584124654e-324, "4.9406564584124654e-324"); /* minimum denormal */
+    TEST_NUMBER(-4.9406564584124654e-324, "-4.9406564584124654e-324");
+    TEST_NUMBER( 2.2250738585072009e-308, "2.2250738585072009e-308");  /* Max subnormal double */
+    TEST_NUMBER(-2.2250738585072009e-308, "-2.2250738585072009e-308");
+    TEST_NUMBER( 2.2250738585072014e-308, "2.2250738585072014e-308");  /* Min normal positive double */
+    TEST_NUMBER(-2.2250738585072014e-308, "-2.2250738585072014e-308");
+    TEST_NUMBER( 1.7976931348623157e+308, "1.7976931348623157e+308");  /* Max double */
+    TEST_NUMBER(-1.7976931348623157e+308, "-1.7976931348623157e+308");
+}
+
+
 static void test_parse_expect_value() {
-    Json_Parse v;
-
-
-    EXPECT_EQ_INT(JSON_PARSE_EXPECT_VALUE, v.json_parse(""));
-    EXPECT_EQ_INT(JSON_NULL, v.json_get_type());
-
-    EXPECT_EQ_INT(JSON_PARSE_EXPECT_VALUE, v.json_parse(" "));
-    EXPECT_EQ_INT(JSON_NULL, v.json_get_type());
+    TEST_ERROR(JSON_PARSE_EXPECT_VALUE,"");
+    TEST_ERROR(JSON_PARSE_EXPECT_VALUE," ");
 }
 
 static void test_parse_invalid_value() {
-    Json_Parse v;
-
-    EXPECT_EQ_INT(JSON_PARSE_INVALID_VALUE, v.json_parse("nul"));
-    EXPECT_EQ_INT(JSON_NULL, v.json_get_type());
-
-
-    EXPECT_EQ_INT(JSON_PARSE_INVALID_VALUE, v.json_parse("?"));
-    EXPECT_EQ_INT(JSON_NULL, v.json_get_type());
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE,"nul");
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE,"?");
+#if  1
+    /* invalid number */
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE, "+0");
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE, "-");
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE, "+1");
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE, ".123"); /* at least one digit before '.' */
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE, "1.");   /* at least one digit after '.' */
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE, "INF");
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE, "inf");
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE, "NAN");
+    TEST_ERROR(JSON_PARSE_INVALID_VALUE, "nan");
+#endif
+#if 1
+    /* too big number */
+    TEST_ERROR(JSON_PARSE_NUMBER_TOO_BIG, "1e309");
+    TEST_ERROR(JSON_PARSE_NUMBER_TOO_BIG, "-1e309");
+#endif
 }
 
 static void test_parse_root_not_singular() {
-    Json_Parse v;
+    TEST_ERROR(JSON_PARSE_ROOT_NOT_SINGULAR,"null x");
 
-    EXPECT_EQ_INT(JSON_PARSE_ROOT_NOT_SINGULAR, v.json_parse("null x"));
-    EXPECT_EQ_INT(JSON_NULL, v.json_get_type());
+#if 1
+    /* invalid number */
+    TEST_ERROR(JSON_PARSE_ROOT_NOT_SINGULAR, "0123"); /* after zero should be '.' , 'E' , 'e' or nothing */
+    TEST_ERROR(JSON_PARSE_ROOT_NOT_SINGULAR, "0x0");
+    TEST_ERROR(JSON_PARSE_ROOT_NOT_SINGULAR, "0x123");
+#endif
 }
 
 static void test_parse() {
     test_parse_null();
     test_parse_true();
     test_parse_false();
+    test_parse_number();
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
