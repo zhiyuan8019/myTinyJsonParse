72c2e0e1f38f00092fb961872efba466fa737181
zhiyuan8019 <2393957799@qq.com>
2021-02-03 17:46:34 +0800

    实现array和object的解析

--- tinyjson.cpp
+++ tinyjson.cpp
@@ -3,20 +3,47 @@
 #include <cctype>
 #include <stdexcept>
 #include <cstring>
+#include <vector>
 
 Json::Json() {
     type = tinyjson::JSON_NULL;
     n = 0;
+    s = "";
+}
+Json::Json(tinyjson::json_type atype) {
+    type =atype;
+    n = 0;
+    s = "";
+}
+Json::Json(std::string astr) {
+    type = tinyjson::JSON_STRING;
+    n=0;
+    s= astr;
+}
+
+Json::Json(double adb) {
+    type = tinyjson::JSON_NUMBER;
+    n = adb;
+}
+
+Json::Json(std::vector<Json> v) {
+    type = tinyjson::JSON_ARRAY;
+    //a = v;
+    n = 0;
+    s = "";
+    a = v;
+}
+Json::Json(std::multimap<std::string, Json> am) {
+    type = tinyjson::JSON_OBJECT;
+    n = 0;
+    s = "";
+    m = am;
 }
 
 Json::~Json() {
-    if(type==tinyjson::JSON_STRING)s.std::string::~string();
 }
 
 void Json::set_type(tinyjson::json_type atype) {
-    if(type ==tinyjson::JSON_STRING){
-        s.std::string::~string();
-    }
     type = atype;
 }
 
@@ -30,14 +57,14 @@ double Json::json_get_number() const {
     }else{
         return -1;
     }
-
 }
 
 void Json::set_num(double adouble) {
-    if(type == tinyjson::JSON_STRING){
-        s.std::string::~string();
-    }
     n = adouble;
+}
+
+void Json::set_array(std::vector<Json> v) {
+    a  = v;
 }
 
 std::string Json::json_get_string() const {
@@ -57,7 +84,7 @@ const char* Json::json_get_c_string() const {
 }
 
 void Json::set_string(std::string astr) {
-    new(&s) std::string (astr);
+    s= astr;
 }
 
 bool Json::json_get_boolean() const {
@@ -68,12 +95,114 @@ bool Json::json_get_boolean() const {
     }
 }
 
+std::vector<Json> Json::json_get_array() const {
+    if(type==tinyjson::JSON_ARRAY){
+        return a;
+    }else{
+        return {};
+    }
+}
+
+Json::Json(const Json &js) {
+    type = js.type;
+    if(type==tinyjson::JSON_NUMBER){
+        n = js.n;
+    }else if(type==tinyjson::JSON_STRING){
+        s= js.s;
+    }else if(type==tinyjson::JSON_ARRAY){
+        a = js.a;
+    }else if(type==tinyjson::JSON_OBJECT){
+        m = js.m;
+    }
+}
+
+Json &Json::operator=(const Json &js) {
+    type = js.type;
+    if(type==tinyjson::JSON_NUMBER){
+        n = js.n;
+    }else if(type==tinyjson::JSON_STRING){
+        s= js.s;
+    }else if(type==tinyjson::JSON_ARRAY){
+        a = js.a;
+    }else if(type==tinyjson::JSON_OBJECT){
+        m = js.m;
+    }
+    return *this;
+}
+
+void Json::clear() {
+    type = tinyjson::JSON_NULL;
+    n = 0;
+    s.clear();
+    a.clear();
+    m.clear();
+}
+
+size_t Json::json_get_object_size() const {
+    return m.size();
+}
+
+std::string Json::json_get_object_key(size_t i) const {
+    if(i<m.size()){
+        auto it = m.begin();
+        while(i>0){
+            --i;
+            ++it;
+        }
+        return it->first;
+    }else{
+        return "";
+    }
+}
+
+const char *Json::json_get_object_key_cstr(size_t i) const {
+    if(i<m.size()){
+        auto it = m.begin();
+        while(i>0){
+            --i;
+            ++it;
+        }
+        char *p = new char[it->first.length()+1];
+        memcpy(p, it->first.c_str(), it->first.length() + 1);
+        return p;
+    }else{
+        return nullptr;
+    }
+}
+
+Json Json::json_get_object_value(size_t i) const{
+    if(i<m.size()){
+        auto it = m.begin();
+        while(i>0){
+            --i;
+            ++it;
+        }
+        return it->second;
+    }else{
+        return Json(tinyjson::JSON_NULL);
+    }
+}
+
+std::multimap<std::string, Json> Json::json_get_object() const {
+    return m;
+}
+
+void Json::set_object(std::multimap<std::string,Json> amap) {
+    m=amap;
+}
+
+Json Json::json_get_object_value_by_key(std::string key) const {
+    auto it = m.find(key);
+    return it->second;
+}
+
 
 int Json_Parse::json_parse(const std::string &js_string) {
     str_buff = &js_string;
     str_size = str_buff->length();
     index  = 0;
     set_type(tinyjson::JSON_NULL);
+    clear();
     if(str_size==0)return tinyjson::JSON_PARSE_EXPECT_VALUE;
     int parse_result;
     parse_ws();
@@ -106,6 +235,8 @@ int Json_Parse::parse_value() {
         case 'f':  return parse_false();
         case 't':  return parse_true();
         case '"':  return parse_string();
+        case '[':  return parse_array();
+        case '{':  return parse_object();
         default:   return parse_number();
     }
 }
@@ -304,4 +435,109 @@ std::string Json_Parse::encode_utf8(unsigned int u) {
         result.push_back(0x80 | ( u        & 0x3F));
     }
     return result;
+}
+
+int Json_Parse::parse_array() {
+    std::vector<Json> v;
+    ++index;
+    if(index>=str_size){
+        set_type(tinyjson::JSON_NULL);
+        return tinyjson::JSON_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+    }
+    parse_ws();
+    if(str_buff->at(index)==']'){
+        ++index;
+        set_array(v);
+        set_type(tinyjson::JSON_ARRAY);
+        return tinyjson::JSON_PARSE_OK;
+    }
+    int ret;
+    while(1){
+        parse_ws();
+        if ((ret = parse_value()) != tinyjson::JSON_PARSE_OK)
+            break;
+        if(json_get_type()==tinyjson::JSON_NULL||json_get_type()==tinyjson::JSON_FALSE||json_get_type()==tinyjson::JSON_TRUE){
+            v.push_back(Json(json_get_type()));
+        }else if(json_get_type()==tinyjson::JSON_NUMBER){
+            v.push_back(Json(json_get_number()));
+        }else if(json_get_type()==tinyjson::JSON_STRING){
+            v.push_back(Json(json_get_string()));
+        }else if(json_get_type()==tinyjson::JSON_ARRAY){
+            v.push_back(Json(json_get_array()));
+        }else if(json_get_type()==tinyjson::JSON_OBJECT){
+            v.push_back(Json(json_get_object()));
+        }
+        parse_ws();
+        if(index<str_size&&str_buff->at(index)==','){
+            ++index;
+            parse_ws();
+        }else if(index<str_size&&str_buff->at(index)==']'){
+            ++index;
+            set_array(v);
+            set_type(tinyjson::JSON_ARRAY);
+            return tinyjson::JSON_PARSE_OK;
+        }else{
+            ret = tinyjson::JSON_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+            break;
+        }
+    }
+    if(ret!=tinyjson::JSON_PARSE_OK)set_type(tinyjson::JSON_NULL);
+    return ret;
+}
+
+int Json_Parse::parse_object() {
+    ++index;
+    int ret;
+    std::multimap<std::string,Json> mmp;
+    parse_ws();
+    if(str_buff->at(index)=='}'){
+        ++index;
+        set_type(tinyjson::JSON_OBJECT);
+        return tinyjson::JSON_PARSE_OK;
+    }
+    while(1){
+        parse_ws();
+        if(index>=str_size||str_buff->at(index)!='"'){
+            ret = tinyjson::JSON_PARSE_MISS_KEY;
+            break;
+        }
+        if ((ret = parse_value()) != tinyjson::JSON_PARSE_OK)
+            break;
+        std::string tmp(json_get_string());
+        parse_ws();
+        if(index>=str_size||str_buff->at(index)!=':'){
+            ret = tinyjson::JSON_PARSE_MISS_COLON;
+            break;
+        }
+        ++index;
+        parse_ws();
+        if ((ret = parse_value()) != tinyjson::JSON_PARSE_OK)
+            break;
+        if(json_get_type()==tinyjson::JSON_NULL||json_get_type()==tinyjson::JSON_FALSE||json_get_type()==tinyjson::JSON_TRUE){
+            mmp.emplace(tmp,Json(json_get_type()));
+        }else if(json_get_type()==tinyjson::JSON_NUMBER){
+            mmp.emplace(tmp,json_get_number());
+        }else if(json_get_type()==tinyjson::JSON_STRING){
+            mmp.emplace(tmp,json_get_string());
+        }else if(json_get_type()==tinyjson::JSON_ARRAY){
+            mmp.emplace(tmp,json_get_array());
+        }else if(json_get_type()==tinyjson::JSON_OBJECT){
+            mmp.emplace(tmp,json_get_object());
+        }
+        parse_ws();
+        if (index<str_size&&str_buff->at(index)==',') {
+            ++index;
+            parse_ws();
+        }else if (index<str_size&&str_buff->at(index)== '}') {
+            ++index;
+            set_type(tinyjson::JSON_OBJECT);
+            set_object(mmp);
+            return tinyjson::JSON_PARSE_OK;
+        }else {
+            ret = tinyjson::JSON_PARSE_MISS_COMMA_OR_CURLY_BRACKET;
+            break;
+        }
+    }
+    if(ret!=tinyjson::JSON_PARSE_OK)set_type(tinyjson::JSON_NULL);
+    return ret;
 }
